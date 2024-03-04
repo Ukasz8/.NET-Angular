fd5ed84cc9f0123daee9c76796fb2e57c7470bc3
Essa <klocek6969696@gmail.com>
2024-03-04 16:39:26 +0100

    1.0

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Entities;
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options) // Końcówka służy do automatycznej inicjalizacji połączenia z bazą na podstawie podanego parametru
+    {
+    }
+    public DbSet<AppUser> Users { get; set; } // Tutaj tworzymy instacje która będzie obsługiwać zapytania naszych użytkowników 
+}
---
+++ API/Program.cs
@@ -0,0 +1,27 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); //Informujemy iż konfiguracja połączenia jest zawarta w owym stringu 
+});
+builder.Services.AddCors();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.MapControllers();
+
+app.Run();
